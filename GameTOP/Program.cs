8e34c524439a94d20733f372bb028b042f22782b
Gabriel <gabrielroodriz@gmail.com>
2020-02-12 17:28:39 -0300

    feat: (Game): Adicionando movimento ao jogador

---
+++ GameTOP/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace GameTOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var jogo = new JogoFODA("Rogue");
+
+            jogo.IniciarJogo();
+        }
+    }
+
+    class JogoFODA
+    {
+        private readonly string _nome;
+
+        public JogoFODA(string nome)
+        {
+            _nome = nome;
+        }
+        public void IniciarJogo()
+        {
+            Console.Write($"{_nome} Passe realizado");
+        }
+    }
+}
